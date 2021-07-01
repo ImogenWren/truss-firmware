ad27b91dcf87ebb893275bb60ae2e5ab2f4c9e6c
dpreid <dprydereid@gmail.com>
2021-07-01 15:29:11 +0100

    reduced the number of gauges to 3 for testing

--- StrainGauge_Peripheral/StrainGauge_Peripheral.ino
+++ StrainGauge_Peripheral/StrainGauge_Peripheral.ino
@@ -10,29 +10,29 @@
 #include "HX711.h"
 #include <Wire.h>
 
-const int numGauges = 7;
+const int numGauges = 3;
 const int GAUGE_0_DT = 3; //DATA pins
 const int GAUGE_1_DT = 4;
 const int GAUGE_2_DT = 5;
-const int GAUGE_3_DT = 6;
-const int GAUGE_4_DT = 7;
-const int GAUGE_5_DT = 8;
-const int GAUGE_6_DT = 9;
+//const int GAUGE_3_DT = 6;
+//const int GAUGE_4_DT = 7;
+//const int GAUGE_5_DT = 8;
+//const int GAUGE_6_DT = 9;
 
-const int data_pins[numGauges] = {GAUGE_0_DT, GAUGE_1_DT, GAUGE_2_DT, GAUGE_3_DT, GAUGE_4_DT, GAUGE_5_DT, GAUGE_6_DT};
-
+//const int data_pins[numGauges] = {GAUGE_0_DT, GAUGE_1_DT, GAUGE_2_DT, GAUGE_3_DT, GAUGE_4_DT, GAUGE_5_DT, GAUGE_6_DT};
+const int data_pins[numGauges] = {GAUGE_0_DT, GAUGE_1_DT, GAUGE_2_DT};
 const int SCK_PIN = 2;  //Common CLOCK pin
 
 HX711 scale_0;
 HX711 scale_1;
 HX711 scale_2;
-HX711 scale_3;
-HX711 scale_4;
-HX711 scale_5;
-HX711 scale_6;
+//HX711 scale_3;
+//HX711 scale_4;
+//HX711 scale_5;
+//HX711 scale_6;
 
-HX711 gaugeScales[numGauges] = {scale_0, scale_1, scale_2, scale_3, scale_4, scale_5, scale_6};
-
+//HX711 gaugeScales[numGauges] = {scale_0, scale_1, scale_2, scale_3, scale_4, scale_5, scale_6};
+HX711 gaugeScales[numGauges] = {scale_0, scale_1, scale_2};
 typedef union
 {
   float number;
@@ -44,19 +44,19 @@ FLOATUNION data[numGauges];
 
 
 void setup() {
-
  initialiseScales();
-
  setGain(128);
  
- gaugeScales[0].set_scale(-15184);   //calibrated with the load cell on the real truss -> OUTPUTS force in newtons
+ gaugeScales[0].set_scale(412);
+ //gaugeScales[0].set_scale(-15184);   //calibrated with the load cell on the real truss -> OUTPUTS force in newtons
  //gaugeScales[1].set_scale(-3231);         //calibrated with truss member 1  -> outputs strain in micro-strain
+ 
   gaugeScales[1].set_scale(-3900);          //member 1
   gaugeScales[2].set_scale(-3900);          //member 2
-  gaugeScales[3].set_scale(-3900);          //member 3
-  gaugeScales[4].set_scale(-3900);          //member 4
-  gaugeScales[5].set_scale(-3900);          //member 5
-  gaugeScales[6].set_scale(-3900);          //member 6
+//  gaugeScales[3].set_scale(-3900);          //member 3
+//  gaugeScales[4].set_scale(-3900);          //member 4
+//  gaugeScales[5].set_scale(-3900);          //member 5
+//  gaugeScales[6].set_scale(-3900);          //member 6
 
   
  tareAllScales();
@@ -65,19 +65,36 @@ void setup() {
  Wire.begin(8);
  Wire.onRequest(requestHandler);
  Wire.onReceive(receiveHandler);
+
+ Serial.begin(57600);
+ while(!Serial);
   
 }
 
 void loop() {
+  
 //  for(int i=0; i<numGauges;i++){
 //    if(gaugeScales[i].is_ready()){
 //      Serial.print("Gauge ");
 //      Serial.print(i);
 //      Serial.print(": \t");
 //      Serial.println(gaugeScales[i].get_units(10));
+//    } else{
+//      Serial.println("Not ready");
 //    }
+//
+//    delay(100);    
 //  }
-  
+
+//  if(scale_0.is_ready()){
+//    Serial.print("Gauge 0: ");
+//    Serial.println(scale_0.get_units(10));
+//  } else{
+//    Serial.println("Not ready");
+//  }
+//  
+//  Serial.println("Waiting..");
+//  delay(1000);
 }
 
 void requestHandler(){
@@ -93,7 +110,7 @@ void requestHandler(){
       
     } else{
       
-      d.number = 0.0;
+      d.number = 1.0;
       data[i] = d;
     }
 
