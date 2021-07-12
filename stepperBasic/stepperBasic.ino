5d5b15040ffac1aa06f73078db99b6d5a7002baf
dpreid <dprydereid@gmail.com>
2021-07-12 12:21:07 +0100

    stepperBasic completed firmware for basic testing of stepper motor on truss experiment

--- stepperBasic/stepperBasic.ino
+++ stepperBasic/stepperBasic.ino
@@ -1,42 +1,58 @@
-//Simple firmware for turning stepper clockwise and anticlockwise
-
-//#include <Stepper.h>
+//Simple firmware for turning stepper clockwise and anticlockwise at different speeds
 
 #define SDIR 2
 #define SPUL 5
 
-#define BUP 8
-#define BDOWN 7
+#define BUP 14
+#define BDOWN 20
 
-const int steps_in_one_rotation = 200;
-const int microsecond_delay = 500;
+#define BSPE 9
 
-//Stepper stepper = Stepper(steps_in_one_rotation, SDIR, SPUL);
+#define LED_SPEED_1 7
+#define LED_SPEED_2 8
+
+int microsecond_delay;
+
+int delays[4] = {1000, 2000, 6000, 10000};
+int speed_index = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+  
   pinMode(BUP, INPUT_PULLDOWN);
   pinMode(BDOWN, INPUT_PULLDOWN);
+  pinMode(BSPE, INPUT_PULLDOWN);
 
   pinMode(SDIR, OUTPUT);
   pinMode(SPUL, OUTPUT);
-  
-  //stepper.setSpeed(stepper_speed);
 
-//  Serial.begin(9600);
-//  while(!Serial);
+  pinMode(LED_SPEED_1, OUTPUT);
+  pinMode(LED_SPEED_2, OUTPUT);
+
+
+  microsecond_delay = delays[speed_index];
+  setSpeedLED(speed_index);
+  
 }
 
 void loop() {
   if(digitalRead(BUP) == HIGH){
-    //Serial.println("UP");
-    step(10);
-  } else if(digitalRead(BDOWN) == HIGH){
-    //Serial.println("DOWN");
-    step(-10);
+    step(1);
+  } 
+  else if(digitalRead(BDOWN) == HIGH){
+    step(-1);
+  } 
+  else if(digitalRead(BSPE) == HIGH){
+    speed_index += 1;
+    
+    if(speed_index == 4){
+      speed_index = 0;
+    }
+    
+    microsecond_delay = delays[speed_index];
+    setSpeedLED(speed_index);
+    
   }
 
-  delay(100);
 
 }
 
@@ -50,18 +66,27 @@ void step(int num_steps){
   
   for (int i=0;i<abs(num_steps);i++){
     digitalWrite(SPUL, HIGH);
-    delayMicroseconds(microsecond_delay);
+    delayMicroseconds((1.0/10.0)*microsecond_delay);
     digitalWrite(SPUL, LOW);
-    delayMicroseconds(microsecond_delay);
+    delayMicroseconds((9.0/10.0)*microsecond_delay);
   }
 }
 
-
-//void rotateFullClockwise(){
-//  stepper.step(steps_in_one_rotation);
-//  
-//}
-//
-//void rotateFullAntiClockwise(){
-//  stepper.step(-steps_in_one_rotation);
-//}
+void setSpeedLED(int index){
+  
+  if(index == 0){
+      digitalWrite(LED_SPEED_1, HIGH);
+      digitalWrite(LED_SPEED_2, HIGH);
+    } else if(index == 1){
+      digitalWrite(LED_SPEED_1, HIGH);
+      digitalWrite(LED_SPEED_2, LOW);
+    } else if(index == 2){
+      digitalWrite(LED_SPEED_1, LOW);
+      digitalWrite(LED_SPEED_2, HIGH);
+    }
+    else if(index == 3){
+      digitalWrite(LED_SPEED_1, LOW);
+      digitalWrite(LED_SPEED_2, LOW);
+    }
+    delay(200);
+}
