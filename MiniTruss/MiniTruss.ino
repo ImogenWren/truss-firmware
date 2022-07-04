27bfb37e67c15d8160dd6957e8036dd95b833abe
dpreid <dprydereid@gmail.com>
2022-07-04 12:20:14 +0100

    updated maximum position to 20 rather than full 100

--- MiniTruss/MiniTruss.ino
+++ MiniTruss/MiniTruss.ino
@@ -31,6 +31,7 @@ char command[COMMAND_SIZE];
 LinearServo servo = LinearServo(DRIVE);
 int currentPos = 0;     //the position of the servo as output from LinearServo library
 int moveToPos = 0;      //the position the servo should move to between 0 (full retraction) to 100 (full extension)
+int max_position = 20;  //% extension of linear actuator
 unsigned long move_interval = 5000L;    //this is updated depending on the move distance
 unsigned long step_interval = 100L;     //at 5V takes approx. 100ms to move 1 position
 unsigned long enter_move_time = millis();    //the time at which the move state started
@@ -395,7 +396,7 @@ StateType readSerialJSON(StateType SmState){
   
         float new_position = doc["to"];
         
-        if(new_position >= 0 && new_position <= 100)
+        if(new_position >= 0 && new_position <= max_position)
         {
           moveToPos = new_position;
           servo.updateMoveTo(moveToPos);
